e37c8e12b3d70456d95c3740e62549710daf87a9
Valirie N. Agbor <56177423+nvagbor@users.noreply.github.com>
2023-12-07 14:10:46 +0000

    Update AlignSNPs.R
    Updated function description

--- AlignSNPs.R
+++ AlignSNPs.R
@@ -1,6 +1,6 @@
 
 # Function to help align SNPs for Mendelian randomisation analysis 
-# Function dependencies: dplyr, rlang 
+# Function dependencies: dplyr and rlang 
 # Function arguments: 
 #     dataset: Dataset containing the exposure (a dataframe or tibble)
 #     var_beta: Name of column containing the betas (Character vector of length 1)
@@ -8,9 +8,9 @@
 #     var_other_allele: Name of column containing the non-effect allele (Character vector of length 1)
 #     var_effect_allele_freq: Name of column containing the minor allele frequency (Character vector of length 1)
 #     positive_beta: Logical vector that determines the sign of the new beta. 
-#                    If TRUE, all negative betas will be flipped to have a negative signs 
+#                    If TRUE, all negative betas will be flipped to have negative signs 
 #                    OR negative sign if FALSE. 
-
+#     The function will preserve the old variables using the suffix ".old" 
 
 
 AlignSNPs <- function(dataset, 
@@ -27,12 +27,7 @@ AlignSNPs <- function(dataset,
                  var_other_allele,
                  var_effect_allele_freq)
   
-  for (k in inputVars) {
-    
-    dataset[ , paste0(k, ".old")] <- dataset[[k]]
-  
-    }
-  
+  for (k in inputVars) { dataset[ , paste0(k, ".old")] <- dataset[[k]] }
   
   if(positive_beta){
     
@@ -46,7 +41,6 @@ AlignSNPs <- function(dataset,
     )
     
   }else{
-    
     df_temp <- dplyr::mutate(
       dataset, 
       pos_beta = positive_beta,
@@ -55,8 +49,7 @@ AlignSNPs <- function(dataset,
       !!var_other_allele := case_when(.data[[var_beta]] > 0 ~ .data[[var_effect_allele]], TRUE ~ .data[[var_other_allele]]),
       !!var_effect_allele_freq := case_when(.data[[var_beta]] > 0 ~ 1 - .data[[var_effect_allele_freq]], TRUE ~ .data[[var_effect_allele_freq]]),
     )
-    
-    
+   
   }
   
   return(df_temp)
