914b2bfcd85e187cb01f2063e8bdaf04f4bcdd78
Valirie N. Agbor <56177423+nvagbor@users.noreply.github.com>
2024-06-11 16:38:44 +0100

    Updated column naming

--- AlignSNPs.R
+++ AlignSNPs.R
@@ -27,22 +27,30 @@ AlignSNPs <- function(dataset,
   for (k in inputVars) { dataset[ , paste0(k, ".old")] <- dataset[[k]] }
   
   if(positive_beta){
+    old_beta <- paste0(var_beta, ".old")
+    old_EA <- paste0(var_effect_allele, ".old")
+    old_AA <- paste0(var_other_allele, ".old")
+    
     df_temp <- dplyr::mutate(
       dataset, 
       pos_beta=positive_beta,
-      !!var_beta:=case_when(.data[[var_beta]]<0~.data[[var_beta]]*-1, TRUE~.data[[var_beta]]), 
-      !!var_effect_allele:=case_when(.data[[var_beta]]<0~.data[[var_other_allele]], TRUE~.data[[var_effect_allele]]),
-      !!var_other_allele:=case_when(.data[[var_beta]]<0~.data[[var_effect_allele]], TRUE~.data[[var_other_allele]]),
-      !!var_effect_allele_freq:=case_when(.data[[var_beta]]<0~1 - .data[[var_effect_allele_freq]], TRUE~.data[[var_effect_allele_freq]]),
+      !!var_beta:=case_when(.data[[old_beta]]<0~.data[[old_beta]]*-1, TRUE~.data[[old_beta]]), 
+      !!var_effect_allele:=case_when(.data[[old_beta]]<0~.data[[old_AA]], TRUE~.data[[old_EA]]),
+      !!var_other_allele:=case_when(.data[[old_beta]]<0~.data[[old_EA]], TRUE~.data[[old_AA]]),
+      !!var_effect_allele_freq:=case_when(.data[[old_beta]]<0~1 - .data[[var_effect_allele_freq]], TRUE~.data[[var_effect_allele_freq]]),
     )
     
   }else{
+    old_beta <- paste0(var_beta, ".old")
+    old_EA <- paste0(var_effect_allele, ".old")
+    old_AA <- paste0(var_other_allele, ".old")
+    
     df_temp <- dplyr::mutate(
       dataset, 
       pos_beta=positive_beta,
       !!var_beta:=case_when(.data[[var_beta]]>0~.data[[var_beta]]*-1, TRUE~.data[[var_beta]]), 
-      !!var_effect_allele:=case_when(.data[[var_beta]]>0~.data[[var_other_allele]], TRUE~.data[[var_effect_allele]]),
-      !!var_other_allele:=case_when(.data[[var_beta]]>0~.data[[var_effect_allele]], TRUE~.data[[var_other_allele]]),
+      !!var_effect_allele:=case_when(.data[[var_beta]]>0~.data[[old_AA]], TRUE~.data[[old_EA]]),
+      !!var_other_allele:=case_when(.data[[var_beta]]>0~.data[[old_EA]], TRUE~.data[[old_AA]]),
       !!var_effect_allele_freq:=case_when(.data[[var_beta]]>0~1 - .data[[var_effect_allele_freq]], TRUE~.data[[var_effect_allele_freq]]),
     )
    
